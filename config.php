bde02dc280ca1257eff633acd2356c58b8cabf8b
Vikas Pandey <vikas@clearitude.com>
2026-09-06 22:53:36 +0530

    Update Facebook profile link

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ return [
     'whatsapp' => '919653203795',
     'instagram_url' => 'https://www.instagram.com/polishandglow_official/',
     'instagram_handle' => '@polishandglow_official',
-    'facebook_url' => 'https://www.facebook.com/share/1Kh4W34vNJ/',
+    'facebook_url' => 'https://www.facebook.com/profile.php?id=61590673050872',
     'facebook_handle' => 'Polish & Glow on Facebook',
     'address' => 'Office No. F-197, First Floor, B-wing, Next to Patel Vanika, Western Express Highway, Goregaon East, Mumbai 400 063.',
 ];
